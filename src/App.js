27fa4169610db2b19107d08cb420079031d2ad45
saiteja-991999 <121968978+saiteja-991999@users.noreply.github.com>
2025-05-17 18:37:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-b
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
